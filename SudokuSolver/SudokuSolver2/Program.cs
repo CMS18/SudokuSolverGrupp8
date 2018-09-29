62fd6552c2b0ad21a990d5ac13f752a4cda1dd0a
Linnea Eriksson <linnea.eriksson91@hotmail.com>
2018-09-29 16:28:30 +0200

    Fixade andra Sudokun

--- SudokuSolver/SudokuSolver2/Program.cs
+++ SudokuSolver/SudokuSolver2/Program.cs
@@ -7,164 +7,66 @@ using System.Threading.Tasks;
 
 namespace SudokuSolver
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    namespace SudokuSolver
     {
-        public static int counter = 0;
-        private const string easy1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
-        private const string easy2 = "619030040270061008000047621486302079000014580031009060005720806320106057160400030";
-        private const string medium1 = "037060000205000800006908000000600024001503600650009000000302700009000402000050360";
-        private const string diabolic1 = "000000000000003085001020000000507000004000100090000000500000073002010000000040009";
-        private const string diabolic2 = "900040000000010200370000005000000090001000400000705000000020100580300000000000000";
-        private const string zen = "000000000000000000000000000000000000000010000000000000000000000000000000000000000";
-        private const string unsolvable1 = "..9.287..8.6..4..5..3.....46.........2.71345.........23.....5..9..4..8.7..125.3..";
-        private const string unsolvable2 = ".9.3....1....8..46......8..4.5.6..3...32756...6..1.9.4..1......58..2....2....7.6.";
-        private const string unsolvable3 = "....41....6.....2...2......32.6.........5..417.......2......23..48......5.1..2...";
-        private const string unsolvable4 = "9..1....4.14.3.8....3....9....7.8..18....3..........3..21....7...9.4.5..5...16..3";
-        private const string unsolvable5 = ".4.1..35.............2.5......4.89..26.....12.5.3....7..4...16.6....7....1..8..2.";
-
-        static void Main(string[] args)
+        class Program
         {
-            int[,] boardArray = new int[9, 9];
-
-            string sudokuString = diabolic1;
-
-            FillBoardArrayWithSudokuString(sudokuString, boardArray);
-            Solve(boardArray);
-            PrintBoardArray(boardArray);
-            Console.WriteLine("Så här långt kom jag på " + counter + " försök");
-            Console.ReadKey();
-        }
-
-        public static bool Solve(int[,] boardArray)
+            static Dictionary<string, string> SudokuTest = new Dictionary<string, string>
         {
-            for (int row = 0; row < 9; row++)
+            {"easy1", "003020600900305001001806400008102900700000008006708200002609500800203009005010300" },
+            {"easy2","619030040270061008000047621486302079000014580031009060005720806320106057160400030"},
+            {"medium1", "037060000205000800006908000000600024001503600650009000000302700009000402000050360" },
+            {"zen", "000000000000000000000000000000000000000010000000000000000000000000000000000000000" },
+            {"unsolvable1", "..9.287..8.6..4..5..3.....46.........2.71345.........23.....5..9..4..8.7..125.3.." },
+            {"unsolvable2", ".9.3....1....8..46......8..4.5.6..3...32756...6..1.9.4..1......58..2....2....7.6." },
+            {"unsolvable3", "....41....6.....2...2......32.6.........5..417.......2......23..48......5.1..2..." },
+            {"unsolvable4", "9..1....4.14.3.8....3....9....7.8..18....3..........3..21....7...9.4.5..5...16..3" },
+            {"unsolvable5", ".4.1..35.............2.5......4.89..26.....12.5.3....7..4...16.6....7....1..8..2." },
+            {"diabolic1", "000000000000003085001020000000507000004000100090000000500000073002010000000040009" },
+            {"diabolic2", "900040000000010200370000005000000090001000400000705000000020100580300000000000000" },
+        };
+
+            static void Main(string[] args)
             {
-                for (int col = 0; col < 9; col++)
+                int[,] sudokuBoard = new int[9, 9];
+
+                foreach (var stringBoard in SudokuTest)//stringboard är elementet "key,value-pair"
                 {
-                    if (boardArray[row, col] == 0)
+                    FillBoardArrayWithSudokuString(stringBoard.Value, sudokuBoard);
+
+                    //Sudoku.PrintBoardArray(boardArray);
+                    Console.WriteLine();
+                    Console.WriteLine("Is " + stringBoard.Key + " solvable?");
+                    Console.WriteLine("---  " + Sudoku.SolveBoard(sudokuBoard) + "  ---");
+                    Console.WriteLine();
+                    Console.WriteLine("----------------------------");
+                }
+
+                Console.ReadLine();
+            }
+
+            private static void FillBoardArrayWithSudokuString(string sudokuString, int[,] sudokuBoard)
+            {
+                int row = -1;
+
+                for (int i = 0; i < sudokuString.Length; i++)
+                {
+                    int num = 0;
+                    if (int.TryParse(sudokuString[i].ToString(), out num) == false)
                     {
-                        counter++;
-                        for (int num = 9; num > 0; num--)
-                        {
-                            string numbers = GetRowColBlockNum(boardArray, row, col);
-                            if (!numbers.Contains(num.ToString()))
-                            {
-                                boardArray[row, col] = num;
-                                if (Solve(boardArray))
-                                {
-                                    return true;
-                                }
-                                boardArray[row, col] = 0;
-                            }
-                        }
-                        return false;
+                        num = 0;
                     }
-                }
-            }
-            return true;
-        }
+                    if (i % 9 == 0) { row++; }
 
-        private static void PrintBoardArray(int[,] boardArray)
-        {
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    if (boardArray[i, j] == 0) //om understreck istället för noll
-                    {
-                        Console.Write("_");
-                    }
-                    else
-                    {
-                        Console.Write(boardArray[i, j]);
-                    }
-
-                    Console.Write(" ");
-
-                    if (j == 2 || j == 5)
-                    {
-                        Console.Write("| ");
-                    }
+                    sudokuBoard[row, i % 9] = num;
                 }
 
-                Console.WriteLine();
-
-                if (i == 2 || i == 5)
-                {
-                    Console.WriteLine("---------------------");
-                }
-            }
-            Console.WriteLine();
-        }
-
-        private static string GetRowColBlockNum(int[,] boardArray, int row, int col)
-        {
-            string allNumbers = "";
-            allNumbers += GetRow(boardArray, row);
-            allNumbers += GetCol(boardArray, col);
-            allNumbers += GetBlock(boardArray, row, col);
-            return allNumbers;
-        }
-
-        private static string GetBlock(int[,] boardArray, int startRow, int startCol)
-        {
-            string numbersInBlock = "";
-
-            startCol = (startCol > -1 && startCol < 3 ? startCol = 0 :
-                        startCol > 2 && startCol < 6 ? startCol = 3 :
-                        startCol > 5 && startCol < 9 ? startCol = 6 : startCol = 100);
-
-            startRow = (startRow > -1 && startRow < 3 ? startRow = 0 :
-                        startRow > 2 && startRow < 6 ? startRow = 3 :
-                        startRow > 5 && startRow < 9 ? startRow = 6 : startRow = 100);
-
-            for (int row = 0; row < 3; row++)
-            {
-                for (int col = 0; col < 3; col++)
-                {
-                    numbersInBlock += boardArray[startRow + row, startCol + col].ToString();
-                }
-            }
-            return numbersInBlock;
-        }
-
-        private static string GetRow(int[,] boardArray, int row)
-        {
-            string numbersInRow = "";
-
-            for (int i = 0; i < 9; i++)
-            {
-                numbersInRow += boardArray[row, i].ToString();
-            }
-
-            return numbersInRow;
-        }
-
-        private static string GetCol(int[,] boardArray, int col)
-        {
-            string numbersInCol = "";
-
-            for (int i = 0; i < 9; i++)
-            {
-                numbersInCol += boardArray[i, col].ToString();
-            }
-
-            return numbersInCol;
-        }
-
-        private static void FillBoardArrayWithSudokuString(string sudokuString, int[,] boardArray)
-        {
-            int row = -1;
-            int test = 0;
-
-            for (int i = 0; i < sudokuString.Length; i++)
-            {
-                if (i % 9 == 0) { row++; }
-                if (int.TryParse(sudokuString[i].ToString(), out test))
-                {
-                    int num = int.Parse(sudokuString[i].ToString());
-                    boardArray[row, i % 9] = num;//fyll varje rad för rad med index 0-8
-                }
             }
         }
     }
---
+++ SudokuSolver/SudokuSolver2/Sudoku.cs
@@ -0,0 +1,245 @@
+﻿namespace SudokuSolver
+{
+    using System;
+    using System.Linq;
+
+    namespace SudokuSolver
+    {
+        public class Sudoku
+        {
+            static string mustContainAllCharacters = "123456789";
+
+            public static void PrintBoardArray(int[,] boardArray)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+
+                        if (boardArray[i, j] == 0) //om understreck istället för noll
+                        {
+                            Console.Write("_");
+                        }
+                        else
+                        {
+                            Console.Write(boardArray[i, j]);
+                        }
+
+                        //Console.Write(boardArray[i, j]);    // om 0 istället för understreck
+
+                        Console.Write(" ");
+
+                        if (j == 2 || j == 5)
+                        {
+                            Console.Write("| ");
+                        }
+                    }
+                    Console.WriteLine();
+
+                    if (i == 2 || i == 5)
+                    {
+                        Console.WriteLine("---------------------");
+
+                    }
+                }
+            }
+
+            public static bool SolveBoard(int[,] sudokuBoard)
+            {
+                Point nextPoint = FindNextUnassigned(sudokuBoard);
+
+                if (nextPoint == null)
+                {
+                    PrintBoardArray(sudokuBoard);
+
+                    //Behöver man verkligen Dubbelkolla?
+                    if (AreAllRowsComplete(sudokuBoard) && AreAllBoxesComplete(sudokuBoard) && AreAllColumnsComplete(sudokuBoard))
+                    {
+                        //Finns det inget kvar att fylla i SolveBoard return true;
+                        return true;
+                    }
+                }
+
+                //Döper om variablerna för att använda kortare namn
+                int row = nextPoint.row;
+                int column = nextPoint.column;
+
+                //Använd alla potentiella siffror som ett sudoku bräde kan innehålla
+                for (int guess = 1; guess <= 9; guess++)
+                {
+                    if (IsNumberAllowed(sudokuBoard, row, column, guess))
+                    {
+                        sudokuBoard[row, column] = guess;
+                        if (SolveBoard(sudokuBoard))//Det här startar själva Rekursionen och testar en "gren" av lösningsalternativ
+                            return true;
+
+                        sudokuBoard[row, column] = 0;//Sätter värdet tillbaka till 0 om lösningen misslyckats
+                    }
+                }
+
+                return false; //backar i trädets gren om en gren av lösningar misslyckats
+            }
+
+            private static Point FindNextUnassigned(int[,] sudokuBoard)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (sudokuBoard[j, i] == 0)
+                        {
+                            return new Point { row = j, column = i };
+                        }
+                    }
+                }
+
+                return null;
+            }
+
+
+            private static bool IsNumberAllowed(int[,] sudokuBoard, int row, int column, int numberToAssign)
+            {
+                var test = sudokuBoard[row, column];
+
+                if (IsAllowedInRow(sudokuBoard, row, numberToAssign) && IsAllowedInColumn(sudokuBoard, column, numberToAssign) && IsAllowedInBox(sudokuBoard, row - row % 3, column - column % 3, numberToAssign))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            private static bool IsAllowedInBox(int[,] sudokuBoard, int boxStartRow, int boxStartCol, int num)
+            {
+                //Tack vare modulu i parametern som skickas in behöver vi bara kolla en 3 * 3 grid
+                for (int row = 0; row < 3; row++)
+                {
+                    for (int col = 0; col < 3; col++)
+                    {
+                        if (sudokuBoard[row + boxStartRow, col + boxStartCol] == num)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+            private static bool IsAllowedInRow(int[,] sudokuBoard, int row, int numberToAssign)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (sudokuBoard[row, i] == numberToAssign)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            private static bool IsAllowedInColumn(int[,] sudokuBoard, int column, int numberToAssign)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (sudokuBoard[i, column] == numberToAssign)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            private static bool AreAllBoxesComplete(int[,] sudokuBoard)
+            {
+                for (int i = 1; i <= 7; i += 3)
+                {
+                    string boxValues = "";
+                    for (int j = 1; j <= 7; j += 3)
+                    {
+                        boxValues = GetBoxValues(i, j, sudokuBoard);
+                        if (!IsRowOrColumnOrBoxComplete(boxValues))
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            private static string GetBoxValues(int i, int j, int[,] sudokuBoard)
+            {
+                string boxValues = "";
+
+                for (int k = -1; k <= 1; k++)
+                {
+                    for (int l = -1; l <= 1; l++)
+                    {
+                        boxValues += sudokuBoard[i + k, j + l];
+                    }
+                }
+
+                return boxValues;
+            }
+
+            private static bool AreAllColumnsComplete(int[,] sudokuBoard)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    string prepareColumns = "";
+                    for (int j = 0; j < 9; j++)
+                    {
+                        prepareColumns += sudokuBoard[j, i].ToString();
+                    }
+                    if (!IsRowOrColumnOrBoxComplete(prepareColumns))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            private static bool AreAllRowsComplete(int[,] sudokuBoard)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    string prepareRow = "";
+                    for (int j = 0; j < 9; j++)
+                    {
+                        prepareRow += sudokuBoard[i, j].ToString();
+                    }
+                    if (!IsRowOrColumnOrBoxComplete(prepareRow))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            private static bool IsRowOrColumnOrBoxComplete(string value)
+            {
+
+                foreach (var c in value)
+                {
+                    int totalOf = value.Count(x => x == c);
+
+                    if (!mustContainAllCharacters.Contains(c.ToString()) && value.Count(x => x == c) != 1)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public class Point
+        {
+            public int row { get; set; }
+            public int column { get; set; }
+        }
+    }
+}
